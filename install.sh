f3c2c17d6e5b98d5d82b01200d22cad4cbf491b4
Diego Alejandro Martínez Cruz <dmarticr@uchicago.edu>
2022-03-08 12:37:03 -0600

    install.sh working

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo -e "1. Creating new virtual environment..."
-
 python -m venv env 
 
 echo -e "2. Installing Requirements..."
 
 source env/bin/activate
 pip install -r requirements.txt
+python crimes_main.py
 
 deactivate 
 echo -e "Install is complete."
