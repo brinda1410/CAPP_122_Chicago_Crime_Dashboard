37f22bc58aedf810147bea9d7aad9fad55fe821b
Diego Alejandro Martínez Cruz <dmarticr@uchicago.edu>
2022-03-08 16:13:52 -0600

    lineal regression

--- linear_reg.py
+++ linear_reg.py
@@ -1,8 +1,31 @@
 import numpy as np
 import pandas as pd
 
+# Creates the Index:
+
 df = pd.read_csv("data/merged_data.csv")
-crimes = ['ARSON', 'ASSAULT', 'BATTERY', 'BURGLARY', 'CONCEALED CARRY LICENSE VIOLATION', 'CRIM SEXUAL ASSAULT', 'CRIMINAL DAMAGE', 'CRIMINAL SEXUAL ASSAULT', 'CRIMINAL TRESPASS', 'DECEPTIVE PRACTICE', 'GAMBLING', 'HOMICIDE', 'HUMAN TRAFFICKING', 'INTERFERENCE WITH PUBLIC OFFICER', 'INTIMIDATION',
- 'KIDNAPPING', 'LIQUOR LAW VIOLATION', 'MOTOR VEHICLE THEFT', 'NARCOTICS', 'NON-CRIMINAL', 'OBSCENITY', 'OFFENSE INVOLVING CHILDREN',
- 'OTHER NARCOTIC VIOLATION', 'OTHER OFFENSE', 'PROSTITUTION', 'PUBLIC INDECENCY', 'PUBLIC PEACE VIOLATION', 'RITUALISM',
- 'ROBBERY', 'SEX OFFENSE', 'STALKING', 'THEFT', 'WEAPONS VIOLATION']
+crimes = ['ARSON', 'ASSAULT', 'BATTERY', 'BURGLARY',\
+          'CONCEALED CARRY LICENSE VIOLATION', 'CRIM SEXUAL ASSAULT',\
+          'CRIMINAL DAMAGE', 'CRIMINAL SEXUAL ASSAULT', 'CRIMINAL TRESPASS',\
+          'DECEPTIVE PRACTICE', 'GAMBLING', 'HOMICIDE', 'HUMAN TRAFFICKING',\
+          'INTERFERENCE WITH PUBLIC OFFICER', 'INTIMIDATION', 'KIDNAPPING',\
+          'LIQUOR LAW VIOLATION', 'MOTOR VEHICLE THEFT', 'NARCOTICS', 'NON-CRIMINAL',\
+          'OBSCENITY', 'OFFENSE INVOLVING CHILDREN', 'OTHER NARCOTIC VIOLATION',\
+          'OTHER OFFENSE', 'PROSTITUTION', 'PUBLIC INDECENCY', 'PUBLIC PEACE VIOLATION',\
+          'RITUALISM', 'ROBBERY', 'SEX OFFENSE', 'STALKING', 'THEFT', 'WEAPONS VIOLATION']
+
+df['y'] = df[crimes].sum(axis=1)
+y = df.loc[:, ['y']].values
+X = df[['FS AGENCIES', 'SB FUNDS', 'MICRO LOANS']].fillna(0)
+X = np.concatenate((X, np.ones((X.shape[0])).reshape(-1, 1)), axis = 1)
+betas = np.linalg.lstsq(X, y,rcond = None)[0]
+error = y - np.matmul(X, betas)
+
+if min(error) >= 0:
+    error = error / max(error)
+else:
+    div = max(max(error), -min(error))
+    error = error / div
+
+df["index"] = error.reshape(1, 50)[0]
+
