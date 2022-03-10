f9adb45363ec3716216b92c7ccafe69db34d16e2
Brinda Sapra <brinda@linux5.cs.uchicago.edu>
2022-03-10 17:26:40 -0600

    change

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo -e "1. Creating new virtual environment..."
-python -m venv env 
+python3 -m venv env 
 
 echo -e "2. Installing Requirements..."
 
 source env/bin/activate
 pip install -r requirements.txt
-python crimes_main.py
+python3 crimes_main.py
 
 deactivate
 rm -r env
